9675bc929acb42f4d7ff885d4410871725856936
software-engineer-caio <contatocaioalvessilva@gmail.com>
2026-02-19 11:04:18 -0300

    final aula de string

--- teste.py
+++ teste.py
@@ -16,3 +16,22 @@ print ('Colocando todo o texto em maiusculo: ',palavra.upper())
 palavra = 'JACA'
 #colocar a string em minusculo
 print ('Colocando todo o texto em minusculo: ',palavra.lower())
+
+#contar caracteres da string
+palavra_contar = 'banana'
+print('Contar a letra b', palavra_contar.count('b'))
+print('Contar a letra a', palavra_contar.count('a'))
+print('Contar a letra n', palavra_contar.count('n'))
+
+#exercicio,gerar senha automaticamente:
+print('Senha gerada automaticamente: ')
+print ('a'+str(email.count('a'))+'e'+str(email.count('e'))+'i'+str(email.count('i'))+'o'+str(email.count('o'))+'u'+str(email.count('u')))
+
+#outra forma de fazer:
+a = email.count('a')
+e = email.count('e')
+i = email.count('i')
+o = email.count('o')
+u = email.count('u')
+print('a'+str(a)+'e'+str(e)+'i'+str(i)+'o'+str(o)+'u'+str(u))
+
