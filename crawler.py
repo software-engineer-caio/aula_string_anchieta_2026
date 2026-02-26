673689b0b1dce9b9a5f155a993f329057caa0073
software-engineer-caio <contatocaioalvessilva@gmail.com>
2026-02-26 09:15:36 -0300

    Primeiro crawler

--- crawler.py
+++ crawler.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 anchieta = 'https://anchieta.br'
 r = requests.get(anchieta)
-html_anchieta = r.text
+    html_anchieta = r.text
 
 soup = BeautifulSoup(html_anchieta)
 for elem in soup.find_all('div',class_='elementor-widget-container'):
